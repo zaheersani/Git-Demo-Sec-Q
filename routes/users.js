dc2a1d4d28cd8f0908f329de58e0da95c227814b
zaheersani <sani.zaheer@gmail.com>
2022-09-19 16:56:54 +0500

    user route restored

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+// const express = require('express');
+const router = express.Router();
+
+const users = [
+    {
+        name: 'John Doe',
+        age: 25
+    },
+    {
+        name: 'David',
+        age: 35
+    },
+    {
+        name: 'Abdullah',
+        age: 23
+    }
+]
+
+router.get('/', (req, res) => {
+    res.send(users)
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    console.log(req.body);
+    users.push(req.body)
+    console.log(users)
+    res.send(users)
+})
+
+export default router;
+
+// module.exports = router;
