f5ecf638ac2c745cc1255d6ca4e8850770398b75
zaheersani <sani.zaheer@gmail.com>
2022-09-19 16:42:27 +0500

    index.js file added

---
+++ index.js
@@ -0,0 +1,32 @@
+//const express = require('express')
+import express from 'express'; // <-- Module Style import
+import bodyParser from 'body-parser';
+import userRouter from './routes/users.js'
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json());
+app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World 3!')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.name)
+    res.send('Default Post Route')
+})
+
+app.get('/todos', (req, res) => {
+    res.send('ToDo Items')
+})
+
+app.post('/user', (req, res) => {
+    console.log(req.body)
+    res.send('ToDo Items')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
